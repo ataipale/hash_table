b705fb2f51e34d03c8b4412a19602b0993035563
Alex Taipale <alextaipale@gmail.com>
2015-04-24 16:28:42 -0400

    Added timing tests, only 300x slower than python's dictionary! D: Still working on getting remove timing method to work.

--- bucketArray.py
+++ bucketArray.py
@@ -1,4 +1,7 @@
 import random
+import timeit
+from textwrap import dedent
+import sys
 
 class bucketArray:
 
@@ -7,18 +10,26 @@ class bucketArray:
 		self.sizeCount = 0
 		self.buckets = self.size * [None]
 
-	# modifies bucketArray to store KVP
-	# xxx check that we aren't getting stuck in a full array? need to?
 	def internal_store(self, KVP, index):
+
+		'''
+		Internal method to modify the bucket array to store KVP
+		'''
+
 		self.sizeCount += 1
 		if index >= self.size:
 			index = 0
-		if self.buckets[index] == None or self.buckets[index] == "Grave Stone":
+		if self.buckets[index] == None or self.buckets[index] == "Grave Stone" or self.buckets[index][0] == KVP[0]:
 			self.buckets[index] = KVP
 		else:
 			self.internal_store(KVP, index+1)
 
 	def internal_fetch(self, key, index):
+
+		'''
+		Method to return value stored at given key
+		'''
+
 		if self.buckets[index] == None:
 			return None
 		elif self.buckets[index][0] == key:
@@ -26,25 +37,36 @@ class bucketArray:
 		else:
 			return self.internal_fetch(key, index+1)
 
-	# If we find none at the insertion site or the following spaces
-	# return "key not found". Otherwise, replace the KVP with a 
-	# Grave Stone.
-	# xxx Do I want remove to remove based just on key term?
-	# Can ppl have duplicate keys? and remove will jsut remove first instance of key?
 	def internal_remove(self, key, index):
+
+		'''
+		If we find none at the insertion site or the following spaces
+		return "key not found". Otherwise, replace the KVP with a 
+		Grave Stone.
+
+		'''
+
 		self.sizeCount -= 1
 		if self.buckets[index] == None:
-			print "Key not found"
+			print "Key Not Found"
 		elif self.buckets[index][0] == key:
+			print "Made Grave Stone"
 			self.buckets[index] = "Grave Stone"
 		else:
+			print "recurse"
 			self.internal_remove(key, index+1)
 
 
 	def setSize(self):
+
+		'''
+		Resize the bucket array to 4x original size when it gets 
+		more than 2/3 full, to avoid time expensive collisions 
+		'''
+
 		if self.sizeCount > self.size*.66:
-			self.buckets += self.size * [None]
-			self.size = self.size * 2
+			self.buckets += (self.size*3) * [None]
+			self.size = self.size * 4
 		return self.size
 
 	def printMe(self):
@@ -52,17 +74,27 @@ class bucketArray:
 
 
 
-# take a KVP and returns an index and KVP 
 
 def findIndex(key, size):
+
+	'''
+	Take a KVP and returns an index to store the KVP in the bucket Array.
+	I use a PRNG seeded on the key value as a hash function to return 
+	a repeatable large hash number. Mod this PRNG number by the size 
+	of the bucket array will give an index within the range of the bucketArray
+	to insert the KVP into.
+	'''
+
 	random.seed(key)
-	# xxx how large should i allow it to choose randInt>?
-	hash_key = random.randint(1, 1e12)
+	hash_key = random.randint(1, 1e32)
 	index = hash_key%size
 	return index
 
-# my class to interact with the user to make dictionary
-class dict:
+class myDict:
+
+	'''
+	My class to interact with the user to make dictionary
+	'''
 
 	def __init__(self):
 		self.bucketArray = bucketArray()
@@ -75,9 +107,8 @@ class dict:
 		index = findIndex(key, self.bucketArray.setSize())
 		return self.bucketArray.internal_fetch(key, index)
 
-	# why is this printing none???????
 	def printMe(self):
-		print self.bucketArray.printMe()
+		self.bucketArray.printMe()
 
 	def remove(self, key):
 		index = findIndex(key, self.bucketArray.setSize())
@@ -85,12 +116,52 @@ class dict:
 
 def main():
 
-	test1 = dict()
-	KVP1 = ('Hamilton', 'Margaret')
-	test1.store(KVP1)
+	# Timing Tests 
+	range_size=500
+	count=500
+	repeat = 3
 
+	def print_results(t):
+		print "Time for %s items stored in %s: " %(str(count), str(t))
+		print t.timeit(number = count)
+		print "Repeated %s times in %s: " %(str(repeat), str(t))
+		print t.repeat(3, count)
+
+
+	setup_statement=dedent('''
+		from bucketArray import myDict;
+		l = [ (str(x), x) for x in range(%d) ];
+		d = myDict()''') % range_size
+	setup_statement_machine='l = [ (str(x), x) for x in range(%d) ]; d = {}' % range_size
+
+	# Test for inputting large number(1000) words
+
+	my_timer = timeit.Timer("for s, i in l: d.store((s, i))",setup_statement)
+	print_results(my_timer)
+	machine_timer = timeit.Timer("for s, i in l: d[s] = i",setup_statement_machine)
+	print_results(machine_timer)
+
+	# Test for getting 1000 items
+
+	my_timer = timeit.Timer("for s in l: d.fetch(s)",setup_statement)
+	print_results(my_timer)
+	machine_timer = timeit.Timer("for s in l: d.get(s)",setup_statement_machine)
+	print_results(machine_timer)	
+
+	# Test for removing 1000 items
+	# xxx Still working on getting below method to work....
+	'''
+	machine_timer = timeit.Timer("for s in l: d.pop(s)",setup_statement_machine)
+	print_results(machine_timer)	
+	my_timer = timeit.Timer("for s in l: d.remove(s)",setup_statement)
+	print_results(my_timer)
+	'''
 
 	# TESTS BELOW
+	test1 = myDict()
+	KVP1 = ('Hamilton', 'Margaret')
+	test1.store(KVP1)
+	print test1
 
 	# test for fetch correctly finding input
 	test_result = test1.fetch(KVP1[0])
@@ -108,6 +179,7 @@ def main():
 		print "input %s , expected 'None'" %str(test_result3)
 
 	# test for inserting something at an already filled spot with same key
+	'''
 	test_result4 = test1.store(('Hamilton', 'Rosalind'))
 	test1.printMe()
 	test1.store(("Go", "Away"))
@@ -130,7 +202,9 @@ def main():
 	test1.printMe()
 	test1.store(("Go", "Away"))
 	test1.printMe()
+	'''
 
 
 if __name__ == '__main__':
-	    main()
+	main()
+
